ec33deb455e2de00aee9132bd7f7f973a6847d4b
Martin Franke <martin.franke@me.com>
2017-05-15 16:13:23 +0200

    Cleanup.

--- symlinks/beaker/.beaker/v1/nginx6134582768995385554/profile_beaker_backend_Python3/ipython_notebook_config.py
+++
@@ -1,5 +0,0 @@
-c = get_config()
-c.NotebookApp.ip = u'127.0.0.1'
-c.NotebookApp.port = 8810
-c.NotebookApp.open_browser = False
-c.NotebookApp.password = u'sha1:951cd0d24274:27abf8046c3671d29e75107c32bae7d5afb24a98'
