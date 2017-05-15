ca5546fd29233366f85df902db8c247b38d54b2b
Martin Franke <martin.franke@me.com>
2017-05-15 15:13:13 +0200

    Added beaker to dotfiles.

---
+++ symlinks/beaker/.beaker/v1/nginx6134582768995385554/profile_beaker_backend_IPython/ipython_notebook_config.py
@@ -0,0 +1,5 @@
+c = get_config()
+c.NotebookApp.ip = u'127.0.0.1'
+c.NotebookApp.port = 8807
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = u'sha1:b0eaae9bd598:7dc524f151d9f0e0518de976e76d03a38d0e2b86'
